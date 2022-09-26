7372e96152426c419f22ab3d4c7650895cbc947f
julio <jc_siles@gmx.ch>
2022-09-26 18:55:16 +0200

    feat(home): add modul and routing

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomepageComponent} from "./pages/homepage/homepage.component";
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomepageComponent
+    path: "tracks",
+    loadChildren: () => import('@modules/tracks/tracks.module').then(m => m.TracksModule)
+  },
+  {
+    path: "favorites",
+    loadChildren: () => import('@modules/favorites/favorites.module').then(m => m.FavoritesModule)
+  },
+  {
+    path: "history",
+    loadChildren: () => import('@modules/history/history.module').then(m => m.HistoryModule)
   }
 ];
 
