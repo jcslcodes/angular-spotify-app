bfdd3f78b32c80f317caf82245197b5fab9d2e21
julio <jc_siles@gmx.ch>
2022-09-26 18:51:04 +0200

    feat(app): add modul and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "@modules/home/pages/homepage/homepage.component";
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
     path: '', //TODO: Info: empty string '' refers to localhost:/4200
+    component: HomepageComponent,
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
   }
 ];
